b6b475b56663ab6fc10c54419ab33b0b2862c64c
Houssem-Ellouze <houssem.ellouze@esprit.tn>
2025-11-07 14:31:39 +0100

    secondCommit

--- microTransfert/src/main/java/tn/esprit/transfert/TransfertApplication.java
+++ microTransfert/src/main/java/tn/esprit/transfert/TransfertApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.transfert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TransfertApplication {
 
     public static void main(String[] args) {
