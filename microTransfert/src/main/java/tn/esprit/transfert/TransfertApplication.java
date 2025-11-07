a82953092f9c2da7844c6065619c53748ba73d05
Houssem-Ellouze <houssem.ellouze@esprit.tn>
2025-11-07 13:39:54 +0100

    first_push

---
+++ microTransfert/src/main/java/tn/esprit/transfert/TransfertApplication.java
@@ -0,0 +1,16 @@
+package tn.esprit.transfert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( TransfertApplication.class, args );
+    }
+
+
+
+
+}
