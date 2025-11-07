8cf068db8625f32def7c870b3f8fe6e8adc1ddc1
firas-mah <firasmahmoudi105@gmail.com>
2025-11-08 00:58:32 +0100

    Save my project

--- microJoueur/src/main/java/com/example/microjoueur/MicroJoueurApplication.java
+++ microJoueur/src/main/java/com/example/microjoueur/MicroJoueurApplication.java
@@ -3,9 +3,11 @@ package com.example.microjoueur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MicroJoueurApplication {
 
 	public static void main(String[] args) {
