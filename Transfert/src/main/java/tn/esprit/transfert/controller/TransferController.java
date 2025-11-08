ca62590e0b39e7ccb947503f7164ec6b7e3f0189
Houssem-Ellouze <houssem.ellouze@esprit.tn>
2025-11-08 14:44:33 +0100

    ee

--- Transfert/src/main/java/tn/esprit/transfert/controller/TransferController.java
+++ Transfert/src/main/java/tn/esprit/transfert/controller/TransferController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("transfers")
 @RequiredArgsConstructor
 public class TransferController {
@@ -97,8 +98,6 @@ public class TransferController {
     }
 
     // -------------------- CLIENT INFO --------------------
-
-
 
     @GetMapping("/clubs")
     public List<Club> getAllClubs() {
